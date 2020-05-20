a7993e030798acfe5e6c125e05901ff7412b3716
Nout Kleef <noutkleef1@gmail.com>
2020-05-20 21:09:00 +0200

    Remove all motion detection
    I have decided to separate motion detection from brightness regulation.
    
    This module will focus on using the sun's position to determine the brightness.

--- MMM-Adaptive-Display.js
+++ MMM-Adaptive-Display.js
@@ -7,19 +7,10 @@ Module.register("MMM-Adaptive-Display", {
 		adjustContrast: true
 	},
 
-	lastScoreDetected: null,
-	lastTimeMotionDetected: null,
-	lastTimePoweredOff: null,
-	percentagePoweredOff: 0,
-	poweredOff: false,
-	poweredOffTime: 0,
-	timeStarted: null,
         contrast: null, // display's contrast
 	brightness: null, // display's brightness
-	illumination: { // correlates to the amount of light in the room
+	illumination: {
 		curr: null,
-	        min: null,
-                max: null
 	},
 	error: null,
 
@@ -29,8 +20,6 @@ Module.register("MMM-Adaptive-Display", {
 
 	getScripts: function () {
 		return [
-			"moment.js",
-			"diff-cam-engine.js"
 		];
 	},
 
@@ -40,82 +29,15 @@ Module.register("MMM-Adaptive-Display", {
 
 	getTemplateData: function () {
 		return {
-			duration: moment.duration(this.poweredOffTime).humanize(),
-			lastScoreDetected: this.lastScoreDetected,
-			lastTimeMotionDetected: this.lastTimeMotionDetected.toLocaleTimeString(),
-			percentagePoweredOff: this.percentagePoweredOff,
 			contrast: this.contrast,
 			brightness: this.brightness,
 			illumination: this.illumination.curr,
-			minIllumination: this.illumination.min,
-			maxIllumination: this.illumination.max,
 			error: this.error
 		};
 	},
 
-	// Override socket notification handler.
-	socketNotificationReceived: function (notification, payload) {
-		if (notification === "USER_PRESENCE") {
-			this.sendNotification(notification, payload);
-		}
-	},
-
 	start: function () {
 		Log.info("MMM-Adaptive-Display: starting up");
-
 		this.data.header = "MMM-Adaptive-Display";
-		this.lastScoreDetected = 0;
-		this.lastTimeMotionDetected = new Date();
-		this.lastTimePoweredOff = new Date();
-		this.timeStarted = new Date().getTime();
-
-		// make sure that the monitor is on when starting
-		this.sendSocketNotification("MOTION_DETECTED", {score: 0});
-
-		const canvas = document.createElement("canvas");
-		const video = document.createElement("video");
-		const cameraPreview = document.createElement("div");
-		cameraPreview.id = "cameraPreview";
-		cameraPreview.style = "visibility:hidden;";
-		cameraPreview.appendChild(video);
-
-		DiffCamEngine.init({
-			video: video,
-			captureIntervalTime: this.config.captureIntervalTime,
-			motionCanvas: canvas,
-			initSuccessCallback: () => {
-				Log.info("MMM-Adaptive-Display: DiffCamEngine init successful");
-				DiffCamEngine.start();
-			},
-			initErrorCallback: (error) => {
-				Log.error("MMM-Adaptive-Display: DiffCamEngine init failed. " + error);
-				this.error = error;
-				this.updateDom();
-			},
-			captureCallback: (payload) => {
-				const score = payload.score;
-				const currentDate = new Date();
-				this.percentagePoweredOff = (100 * this.poweredOffTime / (currentDate.getTime() - this.timeStarted)).toFixed(2);
-				if (score > this.config.scoreThreshold) {
-					if (this.poweredOff) {
-						this.poweredOffTime = this.poweredOffTime + (currentDate.getTime() - this.lastTimePoweredOff.getTime());
-						this.sendSocketNotification("MOTION_DETECTED", {score: score});
-						this.sendNotification("MOTION_DETECTED", {score: score});
-						this.poweredOff = false;
-					}
-					this.lastTimeMotionDetected = currentDate;
-				} else {
-					const time = currentDate.getTime() - this.lastTimeMotionDetected.getTime();
-					if ((time > this.config.timeout) && (!this.poweredOff)) {
-						this.sendSocketNotification("DEACTIVATE_MONITOR", {percentageOff: this.percentagePoweredOff});
-						this.sendNotification("DEACTIVATE_MONITOR", {percentageOff: this.percentagePoweredOff});
-						this.lastTimePoweredOff = currentDate;
-						this.poweredOff = true;
-					}
-				}
-				this.lastScoreDetected = score;
-				this.updateDom();
-			}
-		});
 	}
 });
--- node_helper.js
+++ node_helper.js
@@ -4,51 +4,14 @@ const exec = require("child_process").exec;
 module.exports = NodeHelper.create({
 	start: function () {
 		this.started = false;
-		this.isMonitorOn(function(result) {
-			console.log("MMM-Adaptive-Display: monitor on " + result);
-		});
 	},
 
 	activateMonitor: function () {
-		this.isMonitorOn(function(result) {
-			if (!result) {
-				exec("vcgencmd display_power 1", function(err, out, code) {
-					if (err) {
-						console.error("MMM-Adaptive-Display: error activating monitor: " + code);
-					} else {
-						console.log("MMM-Adaptive-Display: monitor has been activated");
-					}
-				});
-			}
-		});
 		this.started = false;
 	},
 
 	deactivateMonitor: function () {
-		this.isMonitorOn(function(result) {
-			if (result) {
-				exec("vcgencmd display_power 0", function(err, out, code) {
-					if (err) {
-						console.error("MMM-Adaptive-Display: error deactivating monitor: " + code);
-					} else {
-						console.log("MMM-Adaptive-Display: monitor has been deactivated");
-					}
-				});
-			}
-		});
 		this.started = false;
-	},
-
-	isMonitorOn: function(resultCallback) {
-		exec("vcgencmd display_power", function(err, out, code) {
-			if (err) {
-				console.error("MMM-Adaptive-Display: error calling monitor status: " + code);
-				return;
-			}
-
-			console.log("MMM-Adaptive-Display: monitor " + out);
-			resultCallback(out.includes("=1"));
-		});
 	},
 
 	// Subclass socketNotificationReceived received.
